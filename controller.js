0c70ae1e03944e4f18c327593266a7ed1a27ad7d
cat2151 <fct_2004@mail.goo.ne.jp>
2016-03-14 17:52:18 +0900

    #1 URLパラメータを仕様変更し、常にtwitterURL圧縮対象となるようなフォーマットにする を実装したつもり

--- controller.js
+++ controller.js
@@ -2,34 +2,63 @@ angular.module('generatorApp')
 .controller('generatorController', ['$scope', '$location', '$timeout', 'GeneratorService',
 function($scope, $location, $timeout, GeneratorService) {
 
+  $scope.p = {inputText: ''};
   $scope.generatedMml = "なし";
 
-  function setMmlFromUrl() {
-    // [URLイメージ] ～/#?mml=C
-    var urlMml = $location.search().mml;
-    if (angular.isString(urlMml)) {
-      $scope.inputText = urlMml;
-      $scope.generate();
+
+  // URLから取得 [用途] URLだけでMMLを受け取れるようにする
+  function setParamsToScopeFromUrl() {
+    var p = getDecompressedParamsFromUrl();
+    if (p) {
+      $scope.p = p;
     }
   }
+  // URLに反映 [用途] 書いたMMLをURLコピペで共有できるようにする
+  function setParamsToUrlFromScope() {
+    $location.search({
+      p: getCompressedParamsForUrl()
+    });
+  }
+  // [イメージ] $scope.p「aaa」→ URL「～/#?p=bbb」(bbbはlzbase62エンコードされた文字列)
+  function getCompressedParamsForUrl() {
+    return lzbase62.compress(JSON.stringify($scope.p));
+  }
+  // [イメージ] URL「～/#?p=bbb」(bbbはlzbase62エンコードされた文字列) → $scope.p
+  function getDecompressedParamsFromUrl() {
+    var paramFromUrl = $location.search().p;
+    if (!paramFromUrl) return undefined;
+    return JSON.parse(lzbase62.decompress(paramFromUrl));
+  }
+
 
   $scope.generate = function() {
+    $timeout(function() { // compileより前にする(compileがSIOPMロード失敗の為にundefinedでexceptionになっても、先にURLへの反映はしておく)
+      setParamsToUrlFromScope();
+    }, 0);
+
     $scope.generatedMml = $scope.inputText;
     SIOPM.compile($scope.generatedMml);
-    // URLに反映 [用途] 書いたMMLをURLコピペで共有できるようにする
-    $location.search({mml : $scope.generatedMml});
   };
 
+
+
+
   SIOPM.onLoad = function() {
-    $timeout(function() {
-      setMmlFromUrl();
-    }, 1000);
+    if (angular.isString($scope.p.inputText)) {
+      $timeout(function() {
+        $scope.generate();
+      }, 0);
+    }
   };
-        
+
   SIOPM.onCompileComplete = function() {
     SIOPM.play();
   };
 
-  SIOPM.initialize();
+  SIOPM.initialize(); // [前提] SIOPMのプロパティへ各functionを代入し終わっていること
+  $timeout(function() {
+    setParamsToScopeFromUrl(); // [前提] $scopeのプロパティへ各functionを代入し終わっていること
+  }, 0);
+
 
 }]);
